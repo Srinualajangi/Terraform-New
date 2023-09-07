a773365021abf40dfbf81170caf18db910d02fcf
Srinu Alajangi --CNTR <Srinu_Alajangi@contractor.amat.com>
2023-09-07 20:49:51 +0530

    Modified variables.tf

--- ec2.tf
+++ ec2.tf
@@ -2,6 +2,7 @@ resource "aws_instance" "TF-testing" {
     ami = var.ami_id
     instance_type = var.instance_type
     tags = var.tags
+    security_groups = [aws_security_group.Testing.name]
   
 }
     
