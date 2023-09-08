38feaf018041e3d2c3e74c30f8452302fd4cf54e
Srinu Alajangi --CNTR <Srinu_Alajangi@contractor.amat.com>
2023-09-08 10:04:31 +0530

    Added conditions

---
+++ Conditions/conditions.tf
@@ -0,0 +1,5 @@
+resource "aws_instance" "Condition-testing" {
+    ami = var.ami_id
+    instance_type = var.instance_name == "MongoDB" ? "t3.medium" : "t2.micro"
+    tags = var.tags
+}
---
+++ Conditions/variables.tf
@@ -0,0 +1,34 @@
+variable "ami_id" {
+    default = "ami-03265a0778a880afb"    
+}
+
+variable "instance_name" {
+    default = "cart"
+  
+}
+
+variable "instance_type" {
+    default = "t2.micro"
+}
+
+variable "sg_name" {
+    default = "Testing"  
+}
+
+variable "sg_cidr" {
+    type = list
+    default = ["0.0.0.0/0"]
+  
+}
+
+variable "tags" {
+    type = map
+    default = {
+      Name = "cart"
+        Environment   = "DEV"
+        terraform     = "true"
+        Project       = "Roboshop"
+        Component     = "cart"  
+    }
+  
+}
